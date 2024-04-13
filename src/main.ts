03b387e08169abed5235bee0b945ac4cc91bedfe
Daniel Burbano <dburbano@cobiscorp.com>
2024-04-13 15:32:45 -0500

    Change url cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'debug', 'log'] });
   app.enableCors({
-    origin: ["https://miblog.danielburbano.com/", "http:localhost:3000/"]
+    origin: ["https://miblog.danielburbano.com/", "http:localhost:5173/"]
   })
   app.setGlobalPrefix('api');
   await app.listen(3000);
