e407212b7cff8e7347df55bbeeff7d25ac38fc9d
Daniel Burbano <dburbano@cobiscorp.com>
2024-04-13 21:13:52 -0500

    delete dist

--- dist/articles/create-article.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateArticleDto {
-    title: string;
-    description: string;
-    date: Date;
-    image: string;
-    author: string;
-    outstanding: number;
-    text: string;
-}
