1791a276f906f9955a08e4b01aeba71942eac96b
Daniel Burbano <dburbano@cobiscorp.com>
2024-04-13 21:12:56 -0500

    dist

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const whitelist = ["https://miblog.danielburbano.com"];
     var corsOptions = {
-        origin: whitelist,
+        origin: "https://miblog.danielburbano.com",
     };
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { logger: ['error', 'warn', 'debug', 'log'] });
     app.setGlobalPrefix('api');
